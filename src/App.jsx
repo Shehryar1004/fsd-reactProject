fa38d569013f2398139ec40192213ca19126f338
Shehryar <51917712+Shehryar1004@users.noreply.github.com>
2024-01-14 00:12:45 +0500

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { HomePage, AboutPage, ContactPage, BlogPage, Error404Page} from './pages';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='' Component={HomePage}/>
+        <Route path='/about' Component={AboutPage}/>
+        <Route path='/contact' Component={ContactPage}/>
+        <Route path='/blog' Component={BlogPage}/>
+        <Route path='*' Component={Error404Page}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import HomePage from "./Home";
+import BlogPage from './Blog';
+import ContactPage from './Contact';
+import AboutPage from './About';
+import Error404Page from './404';
+
+export {
+    HomePage,
+    AboutPage,
+    BlogPage,
+    ContactPage,
+    Error404Page
+}
